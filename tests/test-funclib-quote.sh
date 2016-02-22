61476159ae601f95bdd6bcc8b6103b5deba6fbe1
Pavel Raiskup <praiskup@redhat.com>
2016-02-22 15:18:10 +0100

    tests: fix file headers
    This code was written by me and I'm fine to relicense according to
    bootstrap rules.
    
    * test-all-shells.sh,modified, test-funclib-quote.sh,
    test-option-parser-helper, test-option-parser.sh: Update
    copyright dates and license statements.

--- tests/test-funclib-quote.sh
+++ tests/test-funclib-quote.sh
@@ -1,22 +1,21 @@
 #! /bin/sh
 
 # Unit tests for funclib.sh
+
+# This is free software.  There is NO warranty; not even for
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-# Copyright (C) 2015 Free Software Foundation, Inc.
-# This file is part of the GNUlib Library.
+# Copyright (C) 2015-2016 Bootstrap Authors
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+# This file is dual licensed under the terms of the MIT license
+# <https://opensource.org/license/MIT>, and GPL version 2 or later
+# <http://www.gnu.org/licenses/gpl-2.0.html>.  You must apply one of
+# these licenses when using or redistributing this software or any of
+# the files within it.  See the URLs above, or the file `LICENSE`
+# included in the Bootstrap distribution for the full license texts.
+
+# Please report bugs or propose patches to:
+# <https://github.com/gnulib-modules/bootstrap/issues>
 
 
 all_shells_script=$0
